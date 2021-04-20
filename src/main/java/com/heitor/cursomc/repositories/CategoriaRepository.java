e24ad39bfcd39b1a11268c641d34337ec3e5df92
HeitorMOliveira <heitorm0rei1@gmail.com>
2021-04-20 02:18:54 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/heitor/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.heitor.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heitor.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	
+}
